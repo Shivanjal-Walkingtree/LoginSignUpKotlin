33f172c1aea590677313d42764827ae1f2a3376b
Shivanjal_walkingtree <shivanjal@walking.tech>
2024-07-05 18:07:27 +0530

    first commit

--- app/src/main/java/com/example/login_registration_app_kt/HomePage.kt
+++ app/src/main/java/com/example/login_registration_app_kt/HomePage.kt
@@ -1,19 +1,33 @@
 package com.example.login_registration_app_kt
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Gravity
+import android.view.View
+import android.view.ViewGroup
+import android.view.Window
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commit
 import com.example.login_registration_app_kt.databinding.ActivityHomePageBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class HomePage : AppCompatActivity() {
 
     private lateinit var binding: ActivityHomePageBinding
+    private lateinit var fab : FloatingActionButton
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        fab = findViewById(R.id.fab)
 
         // Set initial fragment
         if (savedInstanceState == null) {
@@ -40,5 +54,47 @@ class HomePage : AppCompatActivity() {
                 else -> false
             }
         }
+
+        binding.fab.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(p0: View?) {
+                showBottomDialog()
+            }
+        })
+
     }
+
+
+
+    private fun showBottomDialog() {
+        val dialog = Dialog(this)
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(R.layout.bottomsheet_layout)
+
+        val home = dialog.findViewById<LinearLayout>(R.id.home)
+        val me = dialog.findViewById<LinearLayout>(R.id.me)
+        val cancelButton = dialog.findViewById<ImageView>(R.id.cancelButton)
+
+        home.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this, "Upload a Video is clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        me.setOnClickListener {
+            dialog.dismiss()
+            Toast.makeText(this, "Create a short is clicked", Toast.LENGTH_SHORT).show()
+        }
+
+        cancelButton.setOnClickListener {
+            dialog.dismiss()
+        }
+
+        dialog.show()
+        dialog.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        dialog.window?.attributes?.windowAnimations = R.style.DialogAnimation
+        dialog.window?.setGravity(Gravity.BOTTOM)
+    }
+
+
+
 }
